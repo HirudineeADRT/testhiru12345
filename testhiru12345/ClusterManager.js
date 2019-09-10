1307acd7e53490f49e25fd6fe3b7fc189c2f698e
HirudineeADRT <hirudinee@adroitlogic.com>
2019-09-10 15:15:39 +0530

    save

---
+++ testhiru12345/ClusterManager.js
@@ -0,0 +1,9 @@
+module.exports = function() {
+    this.clusters = [];
+
+    this.clusters["hiruclustername"] = {
+        host: "hiruclustername.y6jiqb.ng.0001.use1.cache.amazonaws.com",
+        port: 6379,
+        clusterModeEnabled: false
+    };
+};
--- testhiru12345/lambda.js
+++ testhiru12345/lambda.js
@@ -1,4 +1,19 @@
-exports.handler = function(event, context, callback) {
-    
-    callback(null, {"message": "Successfully executed"});
+let SL_REDIS = require('slappforge-sdk-redis');
+let clusterManager = require('./ClusterManager');
+const redis = new SL_REDIS.Redis(clusterManager);
+
+exports.handler = function (event, context, callback) {
+    // You must always quit the redis client after it's used
+    redis.persist({
+        clusterIdentifier: 'hiruclustername',
+        params: ['gg']
+    }, function (error, response, redisClient) {
+        if (error) {
+            callback(error);
+        } else {
+            //redisClient.quit();
+        }
+    });
+
+    callback(null, { "message": "Successfully executed" });
 }
